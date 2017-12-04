b20c27323339fa699cca343d3b78a2f2fbc83a44
Huy, Nguyen Hong Doan <donghogiacat@yahoo.com.vn>
2017-12-04 13:42:52 +0700

    @ edit search in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,14 @@
     <h2>Garbage</h2>
     <div class="row">
         <div class="col-sm-4">
-            <form action="">
-                <div class="form-group">
-                    <input id="search" type="text" class="form-control" placeholder="Address ...">
+            <div class="form-group">
+                <input id="search" type="text" class="form-control" placeholder="Address ...">
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-8">
+                    <span>Please enter your loction</span>
                 </div>
-                <div class="form-group row">
-                    <div class="col-sm-8">
-                        <span>Please enter your loction</span>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
         <div id="map" class="col-sm-8" garbages="{{ $garbages }}"></div>
     </div>
@@ -23,8 +21,8 @@
 @endsection
 
 @section ('scripts')
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0O6PscWjtS3m6PnfCSdi13Kvkxn18rIo&libraries=places"></script>
-    <script>
-        initMap();
-    </script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0O6PscWjtS3m6PnfCSdi13Kvkxn18rIo&libraries=places"></script>
+<script>
+    initMap();
+</script>
 @endsection
